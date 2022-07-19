a156bff9ff57f1b94fea58b507261e3944349366
josephbill <josephbill00@gmail.com>
2022-07-19 10:04:19 +0300

    added the task and shopper component

--- src/components/CreateTask.component.js
+++ src/components/CreateTask.component.js
@@ -1,11 +1,127 @@
 import React,{Component} from "react";
+import DatePicker from 'react-datepicker'
 
 export default class CreateTaskComponent extends Component{
+
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+
+        this.state = {
+            username: '',
+            duration: '',
+            description: '',
+            users:[]
+        }
+
+    }
+
+    onChangeDate(){
+
+    }
+
+    onSubmit(){
+
+    }
+
+    onChangeDescription(){
+
+    }
+
+    onChangeDuration(){
+
+    }
+
+    onChangeUsername(){
+
+    }
+
+
+
     render(){
         return(
             <div>
-                <p>Paragraph task component</p>
+                <h3>Create task</h3>
+                <div className="form-group">
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <label>Username: </label>
+                            <select
+                            ref="userInput"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                            >
+
+                                <option value="Joseph">Joseph</option>
+                                <option value="Bill">Bill</option>
+
+                            {/*    this data will come from db : options */}
+                            {/*    {*/}
+                            {/*        this.state.users.map(function(user){*/}
+                            {/*            return <option*/}
+                            {/*              key={user}*/}
+                            {/*            >*/}
+                            {/*                {user}*/}
+                            {/*                */}
+                            {/*            </option>*/}
+                            {/*        })*/}
+                            {/*    }*/}
+                            </select>
+                        </div>
+
+                    {/*    description */}
+                        <div className="form-group">
+                            <label>Description: </label>
+                            {/* textarea ,  select , image submission */}
+                            <input
+                                type="text"
+                                required
+                                className="form-control"
+                                value={this.state.description}
+                                onChange={this.onChangeDescription}
+                            />
+                        </div>
+
+                        {/*    description */}
+                        <div className="form-group">
+                            <label>Duration: </label>
+                            {/* textarea ,  select , image submission */}
+                            <input
+                                type="text"
+                                required
+                                className="form-control"
+                                value={this.state.duration}
+                                onChange={this.onChangeDuration}
+                            />
+                        </div>
+
+
+                    {/*    date picker */}
+                        <div className="form-group">
+                            <label>Date: </label>
+                            {/* textarea ,  select , image submission */}
+                            <DatePicker
+                                 selected={this.state.date}
+                                 onChange={this.onChangeDate}
+                            />
+                        </div>
+                        <br/>
+                        <div className="form-group">
+                            <input
+                                type="submit"
+                                value="Create task"
+                                className="btn btn-primary"
+                            />
+                        </div>
+                    </form>
+                </div>
             </div>
-        )
+        );
     }
 }
--- src/components/CreateUser.component.js
+++ src/components/CreateUser.component.js
@@ -1,11 +1,68 @@
 import React,{Component} from "react";
 
 export default class CreateUserComponent extends Component{
+
+    constructor(props) {
+        super(props);
+        //binding methods to this class component
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        //states
+        this.state = {
+            username: ''
+        }
+    }
+
+
+    componentDidMount() {
+
+    }
+
+
+    onSubmit(e){
+       //prevent default entries
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+
+    onChangeUsername(e){
+        //update my state
+        this.setState({
+            username : e.target.value,
+        })
+        console.log(this.state.username)
+    }
+
+
     render(){
         return(
             <div>
-                <p>Paragraph user component</p>
+                <h3>Create User</h3>
+                <form onSubmit={this.onSubmit}>
+                    <br/>
+                    <div className="form-group">
+                         <label>Username: </label>
+                         {/* textarea ,  select , image submission */}
+                         <input
+                         type="text"
+                         required
+                         className="form-control"
+                         value={this.state.username}
+                         onChange={this.onChangeUsername}
+                         />
+                    </div>
+                    <br/>
+                    <div className="form-group">
+                        <input
+                            type="submit"
+                            value="Create User"
+                            className="btn btn-primary"
+                        />
+                    </div>
+                </form>
             </div>
-        )
+        );
     }
 }
